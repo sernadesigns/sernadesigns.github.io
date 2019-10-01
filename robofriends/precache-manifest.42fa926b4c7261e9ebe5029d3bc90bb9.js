0ac8dbb9643504404448e98082706709d789c0eb
sernadesigns <sernadesigns@gmail.com>
2019-09-30 19:48:47 -0700

    Initial build of robofriends

---
+++ robofriends/precache-manifest.42fa926b4c7261e9ebe5029d3bc90bb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff32423b4961f390917ade674add4bf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0c3a6cbb54b2735d6f5",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "e92ddb61228ac3b33cb0",
+    "url": "/static/css/main.96f1c451.chunk.css"
+  },
+  {
+    "revision": "a0c3a6cbb54b2735d6f5",
+    "url": "/static/js/2.371b153a.chunk.js"
+  },
+  {
+    "revision": "e92ddb61228ac3b33cb0",
+    "url": "/static/js/main.884d0eeb.chunk.js"
+  },
+  {
+    "revision": "c2f46d0dcf0b85962d7a",
+    "url": "/static/js/runtime-main.057af00e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
