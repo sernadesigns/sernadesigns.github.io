56e4aa1ff085490fc17b498b36de45d5b94b2610
sernadesigns <sernadesigns@gmail.com>
2020-04-16 00:57:23 -0700

    Updates crown-clothing build

---
+++ crown-clothing/precache-manifest.63f816c36291ed403d6d9f4b70fdebe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627920c07518824038f511c7f332aac2",
+    "url": "/crown-clothing/index.html"
+  },
+  {
+    "revision": "e45aa1d1285f1bb0a482",
+    "url": "/crown-clothing/static/css/main.1f9968c8.chunk.css"
+  },
+  {
+    "revision": "7959780411b5494e4fae",
+    "url": "/crown-clothing/static/js/2.30b25d57.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/crown-clothing/static/js/2.30b25d57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e45aa1d1285f1bb0a482",
+    "url": "/crown-clothing/static/js/main.dd8a7f4e.chunk.js"
+  },
+  {
+    "revision": "884960c478b8c85316b7",
+    "url": "/crown-clothing/static/js/runtime-main.6a9c89e3.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crown-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
